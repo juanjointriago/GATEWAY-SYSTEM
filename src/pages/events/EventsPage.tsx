71b860282cf1de63e0b3157e2efaa7bbce9a32f3
juanintriago <juanjoitoshiki@gmail.com>
2024-05-27 17:09:19 -0500

    feat: table components

--- src/pages/events/EventsPage.tsx
+++ src/pages/events/EventsPage.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
 
 export const EventsPage = () => {
   return (
-    <div>EventsPage</div>
+    <div className="flex flex-col">
+      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+          <div className="overflow-hidden">
+            <table className="min-w-full text-left text-sm font-light">
+              <thead
+                className="border-b bg-white font-medium dark:border-neutral-500 dark:bg-neutral-600">
+                {/*headers */}
+                <tr>
+                  <th scope="col" className="px-6 py-4">#</th>
+                  <th scope="col" className="px-6 py-4">First</th>
+                  <th scope="col" className="px-6 py-4">Last</th>
+                  <th scope="col" className="px-6 py-4">Handle</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* start individual Record */}
+                <tr
+                  className="border-b bg-neutral-100 dark:border-neutral-500 dark:bg-neutral-700">
+                  <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                  <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                  <td className="whitespace-nowrap px-6 py-4">Otto</td>
+                  <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                </tr>
+                {/* end individual Record */}
+
+                <tr
+                  className="border-b bg-neutral-100 dark:border-neutral-500 dark:bg-neutral-700">
+                  <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                  <td
+                    colSpan={2}
+                    className="whitespace-nowrap px-6 py-4 text-center">
+                    Larry the Bird
+                  </td>
+                  <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
