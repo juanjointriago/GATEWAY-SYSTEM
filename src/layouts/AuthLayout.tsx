d6920bd7c69e17de916835b2b96ffff5fb0d89c1
juanintriago <juanjoitoshiki@gmail.com>
2024-07-02 23:08:00 -0500

    feat: add routing pages

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -18,7 +18,7 @@ export const AuthLayout = () => {
   }
   return (
     <div className="bg-gray-100 flex justify-center items-center h-screen">
-      <div className="w-1/2 h-screen hidden lg:flex lg:flex-col items-center justify-center bg-indigo-700">
+      <div className="w-1/2 h-screen hidden lg:flex lg:flex-col items-center justify-center bg-indigo-500">
         {/* <span className="text-white font-bold text-8xl">Gateway Corporation</span> */}
         <img src="https://firebasestorage.googleapis.com/v0/b/gateway-english-iba.appspot.com/o/gateway-assets%2Flogo.png?alt=media&token=1402510d-7ad8-4831-a20e-727191800fcd"
         alt="Placeholder Image"
