82981f7608a052e4b2b6aa25dca757a408c92391
juanintriago <juanjoitoshiki@gmail.com>
2024-07-24 16:37:28 -0500

    fix: validation for editUsers

--- src/components/shared/forms/FormUnit.tsx
+++ src/components/shared/forms/FormUnit.tsx
@@ -124,7 +124,7 @@ export const FormUnit = () => {
               Link de LiveWorkSheet *
             </label>
             <input
-              {...register("workSheetUrl", { required: "La descripción es obligatoria 👀" })}
+              {...register("workSheetUrl")}
               className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="workSheetUrl"
               type="text"
