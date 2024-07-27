d7fffee273d5f4a6e267572e735ab647463e71ef
juanintriago <juanjoitoshiki@gmail.com>
2024-07-27 16:34:39 -0500

    fix: helper.ts for initial letters

--- src/pages/users/helper.ts
+++ src/pages/users/helper.ts
@@ -2,7 +2,7 @@ import { colors } from "../../theme/theme"
 
 export const getInitials = (name: string) => {
     return (name.split(" ").length > 1)
-        ? `${name.split(" ")[0][0].toUpperCase()}${name.split(" ")[1][0].toUpperCase()}`
+        ? `${name.split(" ")[0][0]?? "x".toUpperCase()} ${name.split(" ")[1][0].toUpperCase()}`
         : name ? name.slice(0, 1) : 'XX'
 }
 
