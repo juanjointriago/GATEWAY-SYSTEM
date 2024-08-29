f7c7044da843ac2868a6dd477137d47358b1333b
juanintriago <juanjoitoshiki@gmail.com>
2024-08-28 20:34:02 -0500

    feat: add email message when create reservations

--- src/components/shared/forms/FormEventControl.tsx
+++ src/components/shared/forms/FormEventControl.tsx
@@ -67,32 +67,8 @@ export const FormEventControl: FC = () => {
             const eventRecord = { id: uuid(), ...data }
             //loading swal 
             console.log({ eventRecord });
-            await sendCustomEmail({
-                to: [getUserById(data.teacher!)!.email!],
-                message: {
-                    subject: 'Recordatorio de reservación',
-                    text: `Hola, ${getUserById(data.teacher!)!.name}, se ha asignado un nuevo horario con fecha y hora : ${new Date(eventRecord.date).toLocaleTimeString([], {year: '2-digit', month: "2-digit", day: '2-digit', hour: '2-digit', minute: '2-digit'})} con el nombre de ${eventRecord.name}, con estudiantes de la(s) unidad(es) ${eventRecord.levels[0].subLevels.map(sublevel => sublevels.find(sub => sub.id === sublevel)?.name).join(', ')}, en modalida de ${levels.find((level) => level.id === eventRecord.levels[0].level)?.name}.`,
-                    html: `<h1>Hola, ${getUserById(data.teacher!)!.name}</h1>
-                    <p>Te recordamos que tienes una reservación el ${new Date(eventRecord.date).toLocaleDateString()} a las ${new Date(eventRecord.date).toLocaleTimeString()} con el nombre de ${eventRecord.name}
-                    , con estudiantes de la(s) unidad(es) <b>${eventRecord.levels[0].subLevels.map(sublevel => sublevels.find(sub => sub.id === sublevel)?.name).join(', ')}</b>, en modalidad <b>${levels.find((level) => level.id === eventRecord.levels[0].level)?.name}<b/>.
-                    </p>
-                    <a href="https://gateway-english.com">
-                        <p> © 2024 Gateway Corp derechos reservados </p>
-                        <table width="100%" style="max-width:640px;">
-                        <tr>
-                        <td>
-                        <img width="100%" src='https://firebasestorage.googleapis.com/v0/b/gateway-english-iba.appspot.com/o/gateway-assets%2Flogo.png?alt=media&token=1402510d-7ad8-4831-a20e-727191800fcd'/>
-                          </td>
-                        </tr>
-                      </table>
-                        <br/>
-                        <small>Creado por: Purple-Widget - Software a medida - +(593)987357965</small>
-                        <img style="max-width:100%;height:auto;" src='https://firebasestorage.googleapis.com/v0/b/zustand-practice-e2ec6.appspot.com/o/purplewidgetlogo.png?alt=media&token=9673f9b9-8b45-4ff0-a931-c0e6b4b72f01'/>
-                    </a>`
-                },
 
-            })
-            return;
+            // return;
             Swal.fire({
                 title: 'Creando Reservación',
                 html: 'Espere un momento por favor',
@@ -109,8 +85,33 @@ export const FormEventControl: FC = () => {
                     icon: 'success',
                     confirmButtonColor: '#3085d6',
                     confirmButtonText: 'Aceptar',
-                }).then((result) => {
+                }).then(async (result) => {
                     if (result.isConfirmed) {
+                        await sendCustomEmail({
+                            to: [getUserById(data.teacher!)!.email!],
+                            message: {
+                                subject: 'Recordatorio de reservación',
+                                text: `Hola, ${getUserById(data.teacher!)!.name}, se ha asignado un nuevo horario con fecha y hora : ${new Date(eventRecord.date).toLocaleTimeString([], { year: '2-digit', month: "2-digit", day: '2-digit', hour: '2-digit', minute: '2-digit' })} con el nombre de ${eventRecord.name}, con estudiantes de la(s) unidad(es) ${eventRecord.levels[0].subLevels.map(sublevel => sublevels.find(sub => sub.id === sublevel)?.name).join(', ')}, en modalida de ${levels.find((level) => level.id === eventRecord.levels[0].level)?.name}.`,
+                                html: `<h1>Hola, ${getUserById(data.teacher!)!.name}</h1>
+                                <p>Te recordamos que tienes una reservación el ${new Date(eventRecord.date).toLocaleDateString()} a las ${new Date(eventRecord.date).toLocaleTimeString()} con el nombre de ${eventRecord.name}
+                                , con estudiantes de la(s) unidad(es) <b>${eventRecord.levels[0].subLevels.map(sublevel => sublevels.find(sub => sub.id === sublevel)?.name).join(', ')}</b>, en modalidad <b>${levels.find((level) => level.id === eventRecord.levels[0].level)?.name}<b/>.
+                                </p>
+                                <a href="https://gateway-english.com">
+                                    <p> © 2024 Gateway Corp derechos reservados </p>
+                                    <table width="100%" style="max-width:640px;">
+                                    <tr>
+                                    <td>
+                                    <img width="100%" src='https://firebasestorage.googleapis.com/v0/b/gateway-english-iba.appspot.com/o/gateway-assets%2Flogo.png?alt=media&token=1402510d-7ad8-4831-a20e-727191800fcd'/>
+                                      </td>
+                                    </tr>
+                                  </table>
+                                    <br/>
+                                    <small>Creado por: Purple-Widget - Software a medida - +(593)987357965</small>
+                                    <img style="max-width:100%;height:auto;" src='https://firebasestorage.googleapis.com/v0/b/zustand-practice-e2ec6.appspot.com/o/purplewidgetlogo.png?alt=media&token=9673f9b9-8b45-4ff0-a931-c0e6b4b72f01'/>
+                                </a>`
+                            },
+
+                        })
                         window.location.reload();
                     }
                 })
