ad9681ef6cf494bc5df5600a77ba0b3d1c39c285
juanintriago <juanjoitoshiki@gmail.com>
2025-06-06 22:31:32 -0500

    add describe text for new features

--- src/pages/news/NewsPage.tsx
+++ src/pages/news/NewsPage.tsx
@@ -3,6 +3,7 @@ import { Carousel } from "../../components/shared/carousel/Carousel"
 export const NewsPage = () => {
   return (
     <div>
+      <div>Si es adminsitrador agregar button para redireccionar a la parte adminsitrativa</div>
       <Carousel/>
     </div>
   )
--- src/pages/progress-sheet/PrograssSheetMain.tsx
+++ src/pages/progress-sheet/PrograssSheetMain.tsx
@@ -1,6 +1,6 @@
 
 export const PrograssSheetMain = () => {
   return (
-    <div>PrograssSheetMain</div>
+    <div>Agregar aqui una lista de estudiantes y que cuando escoja uno me redireccione con el uui a la progress-heet de este, si es estudiante debe ver su propio progress sheet</div>
   )
 }
