e7fcd759c6fe707a78b876fe7c3002dac730cbcc
juanintriago <juanjoitoshiki@gmail.com>
2025-08-13 15:34:55 -0500

    temporal fix

--- src/pages/events/EventsPage.tsx
+++ src/pages/events/EventsPage.tsx
@@ -521,6 +521,8 @@ export const EventsPage = () => {
                       (event) =>
                         event.students[user.id!] &&
                         event.isActive 
+                        // &&
+                        // event.levels[0].level === user.level
                     ))) ??
               []
             }
